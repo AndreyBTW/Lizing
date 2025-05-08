f2999934519f5c2e64f9fbe9be1248e11e6d3a2d
AndreyMBTW <andreymbtw@ro.ru>
2025-05-08 17:01:07 +0300

    исправлен калькулятор

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -4,27 +4,33 @@ function formatNumber(num) {
 }
 
 // Функция расчета аннуитетного платежа
-function calculateMonthlyPayment(amount, months, percent) {
-    const getMonthlyPayment = (((amount - percent) * 0.16 / 12 * Math.pow((1 + 0.16 / 12), months)))
+function firstPayment(amount, months, pv) {
+    const firstPay = amount*pv*0.01;
+    return Math.round(firstPay);
+}
+
+
+function calculateMonthlyPayment(amount, months, pv) {
+    const getMonthlyPayment = (((amount - firstPayment(amount, months, pv)) * 0.16 / 12 * Math.pow((1 + 0.16 / 12), months)))
         / (Math.pow(1 + 0.16 / 12, months) - 1);
     return Math.round(getMonthlyPayment);
 }
 
-function calculateTotalPayment(amount, months, percent) {
+function calculateTotalPayment(amount, months, pv) {
 
-    const getTotalPayment = (percent + (calculateMonthlyPayment(amount,months,percent) * months));
+    const getTotalPayment = (firstPayment(amount, months, pv) + (calculateMonthlyPayment(amount,months,pv) * months));
     return Math.round(getTotalPayment);
 }
 
-function calculateSaveNDS(amount, months, percent) {
+function calculateSaveNDS(amount, months, pv) {
 
-    const getSaveNDS = calculateTotalPayment(amount, months, percent) * 0.4;
+    const getSaveNDS = calculateTotalPayment(amount, months, pv) * 0.4;
     return Math.round(getSaveNDS);
 }
 
-function calculateSumWithSaveNDS(amount, months, percent) {
+function calculateSumWithSaveNDS(amount, months, pv) {
 
-    const getSumWithSaveNDS = calculateTotalPayment(amount, months, percent) - calculateSaveNDS(amount, months, percent);
+    const getSumWithSaveNDS = calculateTotalPayment(amount, months, pv) - calculateSaveNDS(amount, months, pv);
     return Math.round(getSumWithSaveNDS);
 }
 
@@ -32,19 +38,23 @@ function calculateSumWithSaveNDS(amount, months, percent) {
 function updateResult() {
     const amount = parseInt(document.getElementById('range1').value);
     const months = parseInt(document.getElementById('range2').value);
-    const percent = parseFloat(document.getElementById('range3').value);
+    const pv = parseFloat(document.getElementById('range3').value);
 
-    const payment = calculateMonthlyPayment(amount, months, percent);
+
+    const payment = calculateMonthlyPayment(amount, months, pv);
     document.getElementById('result').textContent = formatNumber(payment);
 
-    const payment2 = calculateTotalPayment(amount, months, percent);
+    const payment2 = calculateTotalPayment(amount, months, pv);
     document.getElementById('result2').textContent = formatNumber(payment2);
 
-    const payment3 = calculateSaveNDS(amount, months, percent);
+    const payment3 = calculateSaveNDS(amount, months, pv);
     document.getElementById('result3').textContent = formatNumber(payment3);
 
-    const payment4 = calculateSumWithSaveNDS(amount, months, percent);
+    const payment4 = calculateSumWithSaveNDS(amount, months, pv);
     document.getElementById('result4').textContent = formatNumber(payment4);
+
+    const payment5 = firstPayment(amount, months, pv);
+    document.getElementById('result5').textContent = formatNumber(payment5);
 }
 
 // Назначаем обработчики событий
@@ -62,6 +72,7 @@ document.getElementById('range3').addEventListener('input', function() {
     document.getElementById('range3Value').textContent = this.value;
     updateResult();
 });
+
 
 // Инициализация при загрузке
 updateResult();
